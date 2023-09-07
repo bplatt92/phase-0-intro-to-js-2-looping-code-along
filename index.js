c0dc9bfde6f573940d217e5a00e0b275363295cd
Brent Platt <bplatt92@live.com>
2023-09-06 22:20:07 -0400

    looping code along

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(names) {
+    const thankYouMessage = [];
+
+    for (let i = 0; i < names.length; i++) {
+        thankYouMessage.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+
+return thankYouMessage;
+}
+
+function countDown(number) {
+    let countDown = 10
+    while (countDown >= 0) 
+    console.log(countDown--);
+}
